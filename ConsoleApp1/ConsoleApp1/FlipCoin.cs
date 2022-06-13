faea0ce1816272dd756d4e1d381926bebc1313bb
tutorial6 <manapure.vaibhav542@gmail.com>
2022-06-13 19:20:17 +0530

    these Flip Coin Program

---
+++ ConsoleApp1/ConsoleApp1/FlipCoin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicfunctionalprograms
+{
+    public class FlipCoin
+    {
+        public static void Flipcoinpercentage()
+        {
+            Console.WriteLine("Enter the value for No. of Flip Coins");
+
+            int count = 1;
+            int HeadCount = 0;
+            int TailCount = 0;
+
+            double HeadPercentage;
+            double TailPercentage;
+
+            int Flip = Convert.ToInt32(Console.ReadLine());
+
+            if (Flip > 0)
+            {
+                for (int i = 1; i <= Flip; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+                    Console.WriteLine(number);
+
+                    switch (number)
+                    {
+                        case 0:
+                            TailCount++;
+                            break;
+                        case 1:
+                            HeadCount++;
+                            break;
+                    }
+
+
+                }
+                Console.WriteLine("--------------");
+                HeadPercentage = (HeadCount * 100) / Flip;
+                TailPercentage = (TailCount * 100) / Flip;
+
+                Console.WriteLine("Head Percentage : " + HeadPercentage);
+                Console.WriteLine("Tail Percentage : " + TailPercentage);
+
+            }
+            else
+            {
+                Console.WriteLine("Enter Positive Integer");
+            }
+        }
+
+
+
+    }
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Basicfunctionalprograms;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin.Flipcoinpercentage();
+        }
+    }
+}
